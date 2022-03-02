416b591f04d3e4813ae6b764329c4c3d84f134c8
sseong <sseong@deepnoid.com>
2022-03-02 21:47:02 +0900

    port 변경

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:4200'],
     credentials: true,
   });
-  await app.listen(5000);
+  await app.listen(8080);
 }
 bootstrap();
